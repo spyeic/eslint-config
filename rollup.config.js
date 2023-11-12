7b3a8da83f217b4f8fa599541f99dadadf5c87b2
Eric <lderic0415@gmail.com>
2023-11-12 03:42:07 -0800

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import commonjs from "@rollup/plugin-commonjs";
+
+const configNames = ["common", "ts"];
+const config = [];
+
+for (const name of configNames) {
+    config.push({
+        input: `./src/${name}.js`,
+        output: [
+            {
+                file: `dist/cjs/${name}.cjs`,
+                format: "cjs"
+            },
+            {
+                file: `dist/esm/${name}.mjs`,
+                format: "esm"
+            }
+        ],
+        plugins: [
+            commonjs()
+        ]
+    })
+}
+
+export default config;
